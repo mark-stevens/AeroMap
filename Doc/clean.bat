e1f406b827636a7e39b029eb32981524465ecc96
mark-stevens <xmark905@gmail.com>
2024-08-18 14:52:47 -0700

    Initial commit of Doc folder

---
+++ Doc/clean.bat
@@ -0,0 +1,12 @@
+rmdir odm_dem /q /s
+rmdir odm_filterpoints /q /s
+rmdir odm_georeferencing /q /s
+rmdir odm_meshing /q /s
+rmdir odm_orthophoto /q /s
+rmdir odm_report /q /s
+rmdir odm_texturing /q /s
+rmdir odm_texturing_25d /q /s
+rmdir opensfm /q /s
+
+del *.txt
+del *.json
