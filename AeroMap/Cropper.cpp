29f923715623c358c1cc213fe8426d0d56c97afa
mark-stevens <xmark905@gmail.com>
2024-09-04 06:59:36 -0700

    Add more pieces to Cropper.cpp

--- AeroMap/Cropper.cpp
+++ AeroMap/Cropper.cpp
@@ -21,13 +21,18 @@ XString Cropper::path(XString suffix)
 
 XString Cropper::crop(XString gpkg_path, XString geotiff_path, /*gdal_options,*/ bool keep_original /*, warp_options=[]*/)
 {
-	//if not os.path.exists(gpkg_path) or not os.path.exists(geotiff_path):
-	//{
-	//		    log.ODM_WARNING("Either {} or {} does not exist, will skip cropping.".format(gpkg_path, geotiff_path))
-	//    return geotiff_path
-	//}
+	if (QFile::exists(gpkg_path.c_str()) == false)
+	{
+		Logger::Write(__FUNCTION__, "'%s' does not exist, will skip cropping.", gpkg_path.c_str());
+		return geotiff_path;
+	}
+	if (QFile::exists(geotiff_path.c_str()) == false)
+	{
+		Logger::Write(__FUNCTION__, "'%s' does not exist, will skip cropping.", geotiff_path.c_str());
+		return geotiff_path;
+	}
 
-	//log.ODM_INFO("Cropping %s" % geotiff_path)
+	GetApp()->LogWrite("Cropping %s", geotiff_path.c_str());
 
 	// Rename original file
 	// path/to/odm_orthophoto.tif --> path/to/odm_orthophoto.original.tif
@@ -51,16 +56,6 @@ XString Cropper::crop(XString gpkg_path, XString geotiff_path, /*gdal_options,*/
 
 	XString original_geotiff = XString::CombinePath(path, XString::Format("%s.original%s", basename.c_str(), ext.c_str()));
 	AeroLib::Replace(geotiff_path, original_geotiff);
-
-	//try:
-	//    kwargs = {
-	//        'gpkg_path': double_quote(gpkg_path),
-	//        'geotiffInput': double_quote(original_geotiff),
-	//        'geotiffOutput': double_quote(geotiff_path),
-	//        'options': ' '.join(map(lambda k: '-co {}={}'.format(k, gdal_options[k]), gdal_options)),
-	//        'warpOptions': ' '.join(warp_options),
-	//        'max_memory': get_max_memory()
-	//    }
 
 	QStringList args;
 	args.push_back("-cutline");
@@ -88,24 +83,19 @@ XString Cropper::crop(XString gpkg_path, XString geotiff_path, /*gdal_options,*/
 	//			"d:\test_odm\odm_dem\dsm.original.tif"		// geotiff input
 	//			"d:\test_odm\odm_dem\dsm.tif"				// geotiff output
 	//			--config GDAL_CACHEMAX 30.05%
-	//    run('gdalwarp -cutline {gpkg_path} '
-	//        '-crop_to_cutline '
-	//        '{options} '
-	//        '{warpOptions} '
-	//        '{geotiffInput} '
-	//        '{geotiffOutput} '
-	//        '--config GDAL_CACHEMAX {max_memory}%'.format(**kwargs))
 
 	assert(QFile::exists(geotiff_path.c_str()));
 
 	//    if not keep_original:
 	//        os.remove(original_geotiff)
 
-	//except Exception as e:
-	//    log.ODM_WARNING('Something went wrong while cropping: {}'.format(e))
+	if (QFile::exists(geotiff_path.c_str()) == false)
+	{
+		Logger::Write(__FUNCTION__, "Cropping failed.");
 
-	//    # Revert rename
-	//    os.replace(original_geotiff, geotiff_path)
+		// Revert rename
+		AeroLib::Replace(original_geotiff, geotiff_path);
+	}
 
 	return geotiff_path;
 }
@@ -128,31 +118,50 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	// Do decimation prior to extracting boundary information
 	XString decimated_pointcloud_path = path("decimated.las");
 	
-	//    run("pdal translate -i \"{}\" "
-	//        "-o \"{}\" "
-	//        "decimation "
-	//        "--filters.decimation.step={} ".format(pointcloud_path, decimated_pointcloud_path, decimation_step))
-	
-	//    if not os.path.exists(decimated_pointcloud_path):
+	QStringList args;
+	args.push_back("translate");
+	args.push_back("-i");
+	args.push_back(pointcloud_path.c_str());
+	args.push_back("-o");
+	args.push_back(decimated_pointcloud_path.c_str());
+	args.push_back("decimation");
+	args.push_back(XString::Format("--filters.decimation.step=%d", decimation_step).c_str());
+	AeroLib::RunProgramEnv(tree.prog_pdal, args);
+	// cmd: pdal translate -i "d:\test_odm\odm_georeferencing\odm_georeferenced_model.laz"
+	//			-o "d:\test_odm\odm_georeferencing\odm_georeferenced_model.decimated.las" 
+	//			decimation --filters.decimation.step=40 
+	if (QFile::exists(decimated_pointcloud_path.c_str()) == false)
 	{
-		//        log.ODM_WARNING('Could not decimate point cloud, thus cannot generate GPKG bounds {}'.format(decimated_pointcloud_path))
-	//        return ''
+		Logger::Write(__FUNCTION__, "Could not decimate point cloud, thus cannot generate GPKG bounds: '%s'", decimated_pointcloud_path.c_str());
+		return "";
 	}
-	//    # Use PDAL to dump boundary information
-	//    # then read the information back
-	//
-	//    boundary_file_path = self.path('boundary.json')
-	//
-	//    run('pdal info --boundary --filters.hexbin.edge_size=1 --filters.hexbin.threshold=0 "{0}" > "{1}"'.format(decimated_pointcloud_path,  boundary_file_path))
-	//
+	
+	// Use PDAL to dump boundary information
+	// then read the information back
+	
+	XString boundary_file_path = path("boundary.json");
+	
+	QString args;
+	args.push_back("info");
+	args.push_back("--boundary");
+	args.push_back("--filters.hexbin.edge_size=1");
+	args.push_back("--filters.hexbin.threshold=0");
+	args.push_back(decimated_pointcloud_path.c_str());
+	AeroLib::RunProgramEnv(tree.prog_pdal, args, boundary_file_path);
+	// cmd: pdal info 
+	//			--boundary 
+	//			--filters.hexbin.edge_size=1 
+	//			--filters.hexbin.threshold=0 
+	//			"d:\test_odm\odm_georeferencing\odm_georeferenced_model.decimated.las" > "d:\test_odm\odm_georeferencing\odm_georeferenced_model.boundary.json"	
+
 	//    pc_geojson_boundary_feature = None
 	//
 	//    with open(boundary_file_path, 'r') as f:
 	//        json_f = json.loads(f.read())
 	//        pc_geojson_boundary_feature = json_f['boundary']['boundary_json']
-	//
+	
 	//    if pc_geojson_boundary_feature is None: raise RuntimeError("Could not determine point cloud boundaries")
-	//
+	
 	//    # Write bounds to GeoJSON
 	//    tmp_bounds_geojson_path = self.path('tmp-bounds.geojson')
 	//    with open(tmp_bounds_geojson_path, "w") as f:
@@ -163,19 +172,19 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	//                "geometry": pc_geojson_boundary_feature
 	//            }]
 	//        }))
-	//
-	//    # Create a convex hull around the boundary
-	//    # as to encompass the entire area (no holes)
+	
+	// Create a convex hull around the boundary
+	// as to encompass the entire area (no holes)
 	//    driver = ogr.GetDriverByName('GeoJSON')
 	//    ds = driver.Open(tmp_bounds_geojson_path, 0) # ready-only
 	//    layer = ds.GetLayer()
 	//
-	//    # Collect all Geometry
+	// Collect all Geometry
 	//    geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
 	//    for feature in layer:
 	//        geomcol.AddGeometry(feature.GetGeometryRef())
 	//
-	//    # Calculate convex hull
+	// Calculate convex hull
 	//    convexhull = geomcol.ConvexHull()
 	//
 	// If buffer distance is specified
@@ -184,8 +193,9 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	// so that we get smooth corners. \m/
 	const int BUFFER_SMOOTH_DISTANCE = 3;
 	
-	//    if buffer_distance > 0:
-	//        # For small areas, check that buffering doesn't obliterate
+	if (buffer_distance > 0)
+	{
+		//        # For small areas, check that buffering doesn't obliterate
 	//        # our hull
 	//        tmp = convexhull.Buffer(-(buffer_distance + BUFFER_SMOOTH_DISTANCE))
 	//        tmp = tmp.Buffer(BUFFER_SMOOTH_DISTANCE)
@@ -193,7 +203,8 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	//            convexhull = tmp
 	//        else:
 	//            log.ODM_WARNING("Very small crop area detected, we will not smooth it.")
-	//
+	}
+
 	// Save to a new file
 	XString bounds_geojson_path = path("bounds.geojson");
 	//    if os.path.exists(bounds_geojson_path):
@@ -224,27 +235,30 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 
 XString Cropper::create_bounds_gpkg(XString pointcloud_path, double buffer_distance, int decimation_step)
 {
-	//    Compute a buffered polygon around the data extents (not just a bounding box)
-	//    of the given point cloud.
+	// Compute a buffered polygon around the data extents (not just a bounding box)
+	// of the given point cloud.
 	//
-	//    @return filename to Geopackage containing the polygon
-	//    """
-	//    if not os.path.exists(pointcloud_path):
-	//        log.ODM_WARNING('Point cloud does not exist, cannot generate GPKG bounds {}'.format(pointcloud_path))
-	//        return ''
+	// return filename to Geopackage containing the polygon
 	//
-	//    bounds_geojson_path = self.create_bounds_geojson(pointcloud_path, buffer_distance, decimation_step)
-	//
+	
+	if (QFile::exists(pointcloud_path.c_str()) == false)
+	{
+		Logger::Write(__FUNCTION__, "Point cloud does not exist, cannot generate GPKG bounds: '%s'", pointcloud_path.c_str());
+		return "";
+	}
+
+	XString bounds_geojson_path = create_bounds_geojson(pointcloud_path, buffer_distance, decimation_step);
+	
 	//    summary_file_path = XString::CombinePath(m_storage_dir, '{}.summary.json'.format(m_files_prefix))
 	//    export_summary_json(pointcloud_path, summary_file_path)
-	//
+	
 	//    pc_proj4 = None
 	//    with open(summary_file_path, 'r') as f:
 	//        json_f = json.loads(f.read())
 	//        pc_proj4 = json_f['summary']['srs']['proj4']
-	//
+	
 	//    if pc_proj4 is None: raise RuntimeError("Could not determine point cloud proj4 declaration")
-	//
+	
 	XString bounds_gpkg_path = XString::CombinePath(m_storage_dir, XString::Format("%s.bounds.gpkg", m_files_prefix.c_str()));
 
 	//    if os.path.isfile(bounds_gpkg_path):
