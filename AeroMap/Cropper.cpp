f59832e5ca8da209b176ab9c77fee1172a23820e
mark-stevens <xmark905@gmail.com>
2024-09-06 07:11:32 -0700

    Implement parts of Cropper.cpp

--- AeroMap/Cropper.cpp
+++ AeroMap/Cropper.cpp
@@ -2,6 +2,10 @@
 // Port of odm cropper.py
 //
 
+#include <fstream>
+#include <nlohmann/json.h>
+using json = nlohmann::json;
+
 #include "AeroLib.h"
 #include "Cropper.h"
 
@@ -155,61 +159,73 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	//			"d:\test_odm\odm_georeferencing\odm_georeferenced_model.decimated.las" > "d:\test_odm\odm_georeferencing\odm_georeferenced_model.boundary.json"	
 
 	//    pc_geojson_boundary_feature = None
-	//
-	//    with open(boundary_file_path, 'r') as f:
-	//        json_f = json.loads(f.read())
-	//        pc_geojson_boundary_feature = json_f['boundary']['boundary_json']
+
+	std::ifstream f(boundary_file_path.c_str());
+	json data = json::parse(f);
+	json pc_geojson_boundary_feature = data["boundary"]["boundary_json"];
+
+	if (pc_geojson_boundary_feature == nullptr)
+	{
+		Logger::Write(__FUNCTION__, "Could not determine point cloud boundaries");
+		assert(false);
+		return "";
+	}
 	
-	//    if pc_geojson_boundary_feature is None: raise RuntimeError("Could not determine point cloud boundaries")
-	
-	//    # Write bounds to GeoJSON
-	//    tmp_bounds_geojson_path = self.path('tmp-bounds.geojson')
-	//    with open(tmp_bounds_geojson_path, "w") as f:
-	//        f.write(json.dumps({
-	//            "type": "FeatureCollection",
-	//            "features": [{
-	//                "type": "Feature",
-	//                "geometry": pc_geojson_boundary_feature
-	//            }]
-	//        }))
-	
+	// Write bounds to GeoJSON
+	XString tmp_bounds_geojson_path = path("tmp-bounds.geojson");
+	FILE* pFile = fopen(tmp_bounds_geojson_path.c_str(), "wt");
+	if (pFile)
+	{
+		fprintf(pFile, "\"type\": \"FeatureCollection\",\n");
+		fprintf(pFile, "\"features\": [{\n");
+		fprintf(pFile, "	\"type\": \"Feature\",\n");
+		fprintf(pFile, "	\"geometry\": pc_geojson_boundary_feature\n");
+		fprintf(pFile, "	}]\n");
+		fprintf(pFile, "}))\n");
+		fclose(pFile);
+	}
+
 	// Create a convex hull around the boundary
 	// as to encompass the entire area (no holes)
-	//    driver = ogr.GetDriverByName('GeoJSON')
-	//    ds = driver.Open(tmp_bounds_geojson_path, 0) # ready-only
-	//    layer = ds.GetLayer()
-	//
+	GDALDriver* pDriver = static_cast<GDALDriver*>(GDALGetDriverByName("GeoJSON"));
+	assert(pDriver != nullptr);
+
+	GDALDataset* pDS = (GDALDataset*)GDALOpen(tmp_bounds_geojson_path.c_str(), GA_ReadOnly);
+	assert(pDS != nullptr);
+	OGRLayer* pLayer = pDS->GetLayer(0);
+	assert(pLayer != nullptr);
+
 	// Collect all Geometry
 	//    geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
 	//    for feature in layer:
 	//        geomcol.AddGeometry(feature.GetGeometryRef())
-	//
+	
 	// Calculate convex hull
 	//    convexhull = geomcol.ConvexHull()
-	//
+	
 	// If buffer distance is specified
 	// Create two buffers, one shrunk by
 	// N + 3 and then that buffer expanded by 3
-	// so that we get smooth corners. \m/
-	const int BUFFER_SMOOTH_DISTANCE = 3;
+	// so that we get smooth corners.
+	const double BUFFER_SMOOTH_DISTANCE = 3.0;
 	
 	if (buffer_distance > 0)
 	{
-		//        # For small areas, check that buffering doesn't obliterate
-	//        # our hull
-	//        tmp = convexhull.Buffer(-(buffer_distance + BUFFER_SMOOTH_DISTANCE))
-	//        tmp = tmp.Buffer(BUFFER_SMOOTH_DISTANCE)
-	//        if tmp.Area() > 0:
-	//            convexhull = tmp
-	//        else:
-	//            log.ODM_WARNING("Very small crop area detected, we will not smooth it.")
+		// For small areas, check that buffering doesn't obliterate
+		// our hull
+		//        tmp = convexhull.Buffer(-(buffer_distance + BUFFER_SMOOTH_DISTANCE))
+		//        tmp = tmp.Buffer(BUFFER_SMOOTH_DISTANCE)
+		//        if tmp.Area() > 0:
+		//            convexhull = tmp
+		//        else:
+		//            log.ODM_WARNING("Very small crop area detected, we will not smooth it.")
 	}
 
 	// Save to a new file
 	XString bounds_geojson_path = path("bounds.geojson");
-	//    if os.path.exists(bounds_geojson_path):
-	//        os.remove(bounds_geojson_path)
-	
+	if (QFile::exists(bounds_geojson_path.c_str()))
+		QFile::remove(bounds_geojson_path.c_str());
+
 	//    out_ds = driver.CreateDataSource(bounds_geojson_path)
 	//    layer = out_ds.CreateLayer("convexhull", geom_type=ogr.wkbPolygon)
 	
@@ -221,14 +237,17 @@ XString Cropper::create_bounds_geojson(XString pointcloud_path, double buffer_di
 	
 	//    # Save and close data sources
 	//    out_ds = ds = None
+	//GDALClose(out_ds);
 	
-	//    # Remove decimated point cloud
-	//    if os.path.exists(decimated_pointcloud_path):
-	//        os.remove(decimated_pointcloud_path)
+	GDALClose(pDS);
 	
-	//    # Remove tmp bounds
-	//    if os.path.exists(tmp_bounds_geojson_path):
-	//        os.remove(tmp_bounds_geojson_path)
+	// Remove decimated point cloud
+	// if os.path.exists(decimated_pointcloud_path):
+	//     os.remove(decimated_pointcloud_path)
+	
+	// Remove tmp bounds
+	// if os.path.exists(tmp_bounds_geojson_path):
+	//    os.remove(tmp_bounds_geojson_path)
 	
 	return bounds_geojson_path;
 }
@@ -249,15 +268,19 @@ XString Cropper::create_bounds_gpkg(XString pointcloud_path, double buffer_dista
 
 	XString bounds_geojson_path = create_bounds_geojson(pointcloud_path, buffer_distance, decimation_step);
 	
-	//    summary_file_path = XString::CombinePath(m_storage_dir, '{}.summary.json'.format(m_files_prefix))
-	//    export_summary_json(pointcloud_path, summary_file_path)
+	XString summary_file_path = XString::CombinePath(m_storage_dir, XString::Format("%s.summary.json", m_files_prefix.c_str()));
+	//export_summary_json(pointcloud_path, summary_file_path)
 	
-	//    pc_proj4 = None
+	json pc_proj4 = nullptr;
 	//    with open(summary_file_path, 'r') as f:
 	//        json_f = json.loads(f.read())
 	//        pc_proj4 = json_f['summary']['srs']['proj4']
 	
-	//    if pc_proj4 is None: raise RuntimeError("Could not determine point cloud proj4 declaration")
+	if (pc_proj4 == nullptr)
+	{
+		Logger::Write(__FUNCTION__, "Could not determine point cloud proj4 declaration");
+		assert(false);
+	}
 	
 	XString bounds_gpkg_path = XString::CombinePath(m_storage_dir, XString::Format("%s.bounds.gpkg", m_files_prefix.c_str()));
 
@@ -276,54 +299,57 @@ XString Cropper::create_bounds_gpkg(XString pointcloud_path, double buffer_dista
 	return bounds_gpkg_path;
 }
 
-//@staticmethod
-//def merge_bounds(input_bound_files, output_bounds, double buffer_distance = 0):
-//    """
-//    Merge multiple bound files into a single bound computed from the convex hull
-//    of all bounds (minus a buffer distance in meters)
-//    """
-//    geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
-//
-//    driver = ogr.GetDriverByName('GPKG')
-//    srs = None
-//
-//    for input_bound_file in input_bound_files:
-//        ds = driver.Open(input_bound_file, 0) # ready-only
-//
-//        layer = ds.GetLayer()
-//        srs = layer.GetSpatialRef()
-//
-//        # Collect all Geometry
-//        for feature in layer:
-//            geomcol.AddGeometry(feature.GetGeometryRef())
-//
-//        ds = None
-//
-//    # Calculate convex hull
-//    convexhull = geomcol.ConvexHull()
-//
-//    # If buffer distance is specified
-//    # Create two buffers, one shrunk by
-//    # N + 3 and then that buffer expanded by 3
-//    # so that we get smooth corners. \m/
-//    BUFFER_SMOOTH_DISTANCE = 3
-//
-//    if buffer_distance > 0:
-//        convexhull = convexhull.Buffer(-(buffer_distance + BUFFER_SMOOTH_DISTANCE))
-//        convexhull = convexhull.Buffer(BUFFER_SMOOTH_DISTANCE)
-//
-//    # Save to a new file
-//    if os.path.exists(output_bounds):
-//        driver.DeleteDataSource(output_bounds)
-//
-//    out_ds = driver.CreateDataSource(output_bounds)
-//    layer = out_ds.CreateLayer("convexhull", srs=srs, geom_type=ogr.wkbPolygon)
-//
-//    feature_def = layer.GetLayerDefn()
-//    feature = ogr.Feature(feature_def)
-//    feature.SetGeometry(convexhull)
-//    layer.CreateFeature(feature)
-//    feature = None
-//
-//    # Save and close output data source
-//    out_ds = None
+void Cropper::merge_bounds(XString input_bound_files, XString output_bounds, double buffer_distance)
+{
+	// Merge multiple bound files into a single bound computed from the convex hull
+	// of all bounds (minus a buffer distance in meters)
+	//
+	
+	//geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
+	//
+	//driver = ogr.GetDriverByName('GPKG')
+	//srs = None
+	//
+	//for input_bound_file in input_bound_files:
+	//    ds = driver.Open(input_bound_file, 0) # ready-only
+	//
+	//    layer = ds.GetLayer()
+	//    srs = layer.GetSpatialRef()
+	//
+	//    # Collect all Geometry
+	//    for feature in layer:
+	//        geomcol.AddGeometry(feature.GetGeometryRef())
+	//
+	//    ds = None
+	//
+	//# Calculate convex hull
+	//convexhull = geomcol.ConvexHull()
+	//
+	//# If buffer distance is specified
+	//# Create two buffers, one shrunk by
+	//# N + 3 and then that buffer expanded by 3
+	//# so that we get smooth corners. \m/
+	const double BUFFER_SMOOTH_DISTANCE = 3.0;
+	
+	//    if buffer_distance > 0:
+	{
+		//        convexhull = convexhull.Buffer(-(buffer_distance + BUFFER_SMOOTH_DISTANCE))
+	//        convexhull = convexhull.Buffer(BUFFER_SMOOTH_DISTANCE)
+	}
+
+	//# Save to a new file
+	//if os.path.exists(output_bounds):
+	//    driver.DeleteDataSource(output_bounds)
+	//
+	//out_ds = driver.CreateDataSource(output_bounds)
+	//layer = out_ds.CreateLayer("convexhull", srs=srs, geom_type=ogr.wkbPolygon)
+	//
+	//feature_def = layer.GetLayerDefn()
+	//feature = ogr.Feature(feature_def)
+	//feature.SetGeometry(convexhull)
+	//layer.CreateFeature(feature)
+	//feature = None
+	//
+	//# Save and close output data source
+	//out_ds = None
+}
--- AeroMap/Cropper.h
+++ AeroMap/Cropper.h
@@ -13,7 +13,8 @@ public:
 	XString create_bounds_geojson(XString pointcloud_path, double buffer_distance = 0, int decimation_step = 40);
 	XString create_bounds_gpkg(XString pointcloud_path, double buffer_distance = 0, int decimation_step = 40);
 
-	static XString Cropper::crop(XString gpkg_path, XString geotiff_path, /*gdal_options,*/ bool keep_original = true/*, warp_options=[]*/);
+	static XString crop(XString gpkg_path, XString geotiff_path, /*gdal_options,*/ bool keep_original = true/*, warp_options=[]*/);
+	static void merge_bounds(XString input_bound_files, XString output_bounds, double buffer_distance = 0);
 
 private:
 	XString m_storage_dir;
