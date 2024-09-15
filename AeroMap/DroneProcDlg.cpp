11561f636e1e0de1f2b6e7511d2039e796f17544
mark-stevens <xmark905@gmail.com>
2024-09-15 10:58:41 -0700

    Add point cloud parameters to UI

--- AeroMap/DroneProcDlg.cpp
+++ AeroMap/DroneProcDlg.cpp
@@ -30,11 +30,26 @@ DroneProcDlg::DroneProcDlg(QWidget* parent)
 	cboInitStage->addItem("Orthophoto", (int)Stage::Id::Orthophoto);
 	cboInitStage->addItem("Report", (int)Stage::Id::Report);
 
+	cboPcQuality->clear();
+	cboPcQuality->addItem("ultra");
+	cboPcQuality->addItem("high");
+	cboPcQuality->addItem("medium");
+	cboPcQuality->addItem("low");
+	cboPcQuality->addItem("lowest");
+
 	// load defaults
+
 	spinCrop->setValue(arg.crop);
 	spinDemResolution->setValue(arg.dem_resolution);
 	spinOrthoResolution->setValue(arg.orthophoto_resolution);
 	spinGapFill->setValue(arg.dem_gapfill_steps);
+	
+	chkPcClassify->setChecked(arg.pc_classify);
+	chkPcRectify->setChecked(arg.pc_rectify);
+	spinPcFilter->setValue(arg.pc_filter);
+	spinPcSample->setValue(arg.pc_sample);
+	cboPcQuality->setCurrentText(arg.pc_quality.c_str());
+
 	chkDSM->setChecked(arg.dsm);
 	chkDTM->setChecked(arg.dtm);
 
@@ -58,6 +73,13 @@ void DroneProcDlg::OnRun()
 	arg.dem_resolution = spinDemResolution->value();
 	arg.orthophoto_resolution = spinOrthoResolution->value();
 	arg.dem_gapfill_steps = spinGapFill->value();
+
+	arg.pc_classify = chkPcClassify->isChecked();
+	arg.pc_rectify = chkPcRectify->isChecked();
+	arg.pc_filter = spinPcFilter->value();
+	arg.pc_sample = spinPcSample->value();
+	arg.pc_quality = cboPcQuality->currentText();
+
 	arg.dsm = chkDSM->isChecked();
 	arg.dtm = chkDTM->isChecked();
 
